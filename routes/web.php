c7d936c11a91c8da04c5023198cb2f52c1075bf2
Fathima Salma Muzammil <salmamuzam77@gmail.com>
2025-11-29 10:39:28 +0530

    Designed a responsive home page for customers

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
-    return view('auth.login');
+    return view('welcome');
 });
 
 Route::middleware([
